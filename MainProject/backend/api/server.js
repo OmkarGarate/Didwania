c18f177248e58b542118b92d6be5b43c48afcb0b
Omkar Garate <garateomkar143@gmail.com>
2024-10-07 15:08:01 +0530

    added dotenv

--- MainProject/backend/api/server.js
+++ MainProject/backend/api/server.js
@@ -1,7 +1,7 @@
+require('dotenv').config(); 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
 const bodyParser = require('body-parser');
 const userRoutes = require('../routes/userRoutes')
 const productRoutes = require('../routes/productRoutes')
